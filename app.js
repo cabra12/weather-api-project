d46757eea7504380b7a0db214453b3dc840ecc89
cabrah13 <charlottemabraham32@gmail.com>
2026-02-25 20:08:54 -0500

    setup HTML structure, and taking input in JS file

---
+++ app.js
@@ -0,0 +1,7 @@
+const submitButton = document.getElementById('submit-button');
+const resultsArea = document.getElementById('results');
+const cityInput = document.getElementById('city');
+
+submitButton.addEventListener('click', () => {
+    const cityValue = cityInput.value;
+});
