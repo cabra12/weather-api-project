97c8e7538919d6bd1760672576f43453c3ddabb8
cabrah13 <charlottemabraham32@gmail.com>
2026-04-02 12:01:14 -0400

    change style and add more API calls

--- app.js
+++ app.js
@@ -8,15 +8,32 @@ const getCity = () => {
 
 const fetchAPIData = async (city) => {
     const key = 'ea12bceac54c6a464611413684fbe029';
-    const API_URL = 'https://api.openweathermap.org/data/2.5/weather';
-    const response = await fetch(`${API_URL}?q=${city}&appid=${key}&units=imperial`);
+    const base_API_URL = 'https://api.openweathermap.org/data/2.5';
 
-    const data = await response.json();
-    createCard(data);
+    const locationResponse = await fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${encodeURIComponent(city)}&limit=1&appid=${key}`);
+    const locationData= await locationResponse.json();
+    const lat = locationData[0].lat;
+    const lon = locationData[0].lon;
+
+    const [currentRes, forecastRes] = await Promise.all([
+        fetch(`${base_API_URL}/weather?q=${city}&appid=${key}&units=imperial`),
+        fetch(`${base_API_URL}/forecast?lat=${lat}&lon=${lon}&cnt=40&appid=${key}&units=imperial`),
+    ]);
+
+    const current = await currentRes.json();
+    const forecast = await forecastRes.json();
+
+    createCard(current, forecast);
 };
 
 const createCard = (data) => {
-    
+    const div = document.createElement('div');
+    div.classList.add('weather-card');
+    div.innerHTML = `
+
+    `;
+
+    document.querySelector('.weather-cards-container').appendChild(div);
 };
 
 //Event Listeners
