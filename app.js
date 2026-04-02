601736a277c311abeca268daa0431e9b2b4ed24a
cabrah13 <charlottemabraham32@gmail.com>
2026-04-02 10:33:29 -0400

    added async programming to retrieve weather data

--- app.js
+++ app.js
@@ -1,7 +1,24 @@
 const submitButton = document.getElementById('submit-button');
-const resultsArea = document.getElementById('results');
 const cityInput = document.getElementById('city');
 
-submitButton.addEventListener('click', () => {
-    const cityValue = cityInput.value;
-});
+const getCity = () => {
+    const city = cityInput.value;
+    fetchAPIData(city);
+};
+
+const fetchAPIData = async (city) => {
+    const key = 'ea12bceac54c6a464611413684fbe029';
+    const API_URL = 'https://api.openweathermap.org/data/2.5/weather';
+    const response = await fetch(`${API_URL}?q=${city}&appid=${key}&units=imperial`);
+
+    const data = await response.json();
+    createCard(data);
+};
+
+const createCard = (data) => {
+    
+};
+
+//Event Listeners
+submitButton.addEventListener('click', getCity);
+
