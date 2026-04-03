5f11f93cd6b60bdd0b372625011f4ffb1eabd5e2
cabrah13 <charlottemabraham32@gmail.com>
2026-04-03 12:14:30 -0400

    add functionality to dynamically add city weather to the DOM, current and forecast values

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
+//https://openweathermap.org/current?collection=current_forecast#name
+//https://openweathermap.org/forecast5?collection=current_forecast#5days
+
 const submitButton = document.getElementById('submit-button');
 const cityInput = document.getElementById('city');
 
 const getCity = () => {
     const city = cityInput.value;
     fetchAPIData(city);
+    cityInput.value = '';
 };
 
 const fetchAPIData = async (city) => {
@@ -23,18 +27,88 @@ const fetchAPIData = async (city) => {
     const current = await currentRes.json();
     const forecast = await forecastRes.json();
 
-    createCard(current, forecast);
+    const forecastArray = getDailyForecast(forecast);
+
+    createCard(current, forecastArray);
+    console.log(current);
 };
 
-const createCard = (data) => {
-    const div = document.createElement('div');
-    div.classList.add('weather-card');
-    div.innerHTML = `
+const getDay = (dateStr) => {
+    const date = new Date(dateStr.replace(/-/g, "/"));
+    return date.toLocaleDateString("en-US", { weekday: "short"});
+}
 
+const getDailyForecast = (forecast) => {
+    const days = {};
+
+    forecast.list.forEach(entry => {
+        const date = entry.dt_txt.split(" ")[0];
+        
+        if(!days[date]) {
+            days[date] = {
+                day: getDay(date),
+                temps_max: [],
+                temps_min: [],
+                weather: entry.weather[0].main,
+            }
+        }
+
+        if(entry.dt_txt.includes('12:00:00')) {
+            days[date].weather = entry.weather[0].main;
+        }
+
+        days[date].temps_max.push(entry.main.temp_max);
+        days[date].temps_min.push(entry.main.temp_min);
+    });
+
+    return days;
+};
+
+const getDyanmicDay = () => {
+    const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+    return days[new Date().getDay()];
+};
+
+const forecastHTML = (forecast) => {
+    return Object.values(forecast).map(day =>
+        `<div class="forecast-day">
+            <span class="day-label">${day.day}</span>
+            <div class="forecast-icon"><!-- img --></div>
+            <div class="forecast-temps">
+                <span class="lo">${Math.round(Math.min(...day.temps_min))}&deg;</span>
+                <span class="hi">${Math.round(Math.max(...day.temps_max))}&deg;</span> 
+            </div>
+        </div>`
+    ).join("");
+};
+
+const createCard = (current, forecast) => {
+
+    const div = document.createElement('div');
+    div.classList.add('weather-input-container');
+    div.innerHTML = `
+                <div class="main-row">
+                    <div class="city-block">
+                        <h2 class="city-name">${current.name}</h2>
+                        <div class="condition">${current.weather[0].main} Min: ${Math.round(current.main.temp_min)}°F &nbsp; Max: ${Math.round(current.main.temp_max)}°F </div>
+                        <div class="temp-display">${Math.round(current.main.temp)}°F</span></div>
+                    </div>
+            
+                    <div class="weather-icon">
+                    </div>
+            
+                    <div class="details-block">
+                        <p>${getDyanmicDay()} ${new Date().toLocaleTimeString("en-US", { hour: "numeric", minute: "2-digit"})}</p>
+                        <p>Humidity: ${current.main.humidity}%</p>
+                        <p>Wind: ${current.wind.speed} mi/h</p>
+                    </div>
+                </div>
+                <div class="forecast-row"> ${forecastHTML(forecast)}</div> 
     `;
 
-    document.querySelector('.weather-cards-container').appendChild(div);
+    document.querySelector('.card').appendChild(div);
 };
+
 
 //Event Listeners
 submitButton.addEventListener('click', getCity);
