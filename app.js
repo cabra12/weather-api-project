03e5e706793f7db432a179433d3a474f0c48da71
cabrah13 <charlottemabraham32@gmail.com>
2026-04-05 10:44:51 -0400

    add spinner and styling with card and units

--- app.js
+++ app.js
@@ -1,21 +1,19 @@
-//https://openweathermap.org/weather-conditions <------icons
-//https://openweathermap.org/forecast5?collection=current_forecast#5days
-
 const submitButton = document.getElementById('submit-button');
 
 let rawInput;
 let lastQuery;
-
+//variable named this way when user toggles, the "last query" is used in the API
 
 const getLocation = (rawInput) => {
     //this was done to make it more relevant outside of the US
     const US_STATES = ['AL','AK','AZ','AR','CA','CO','CT','DE','FL','GA','HI','ID','IL','IN','IA','KS','KY','LA','ME','MD','MA','MI','MN','MS','MO','MT','NE','NV','NH','NJ','NM','NY','NC','ND','OH','OK','OR','PA','RI','SC','SD','TN','TX','UT','VT','VA','WA','WV','WI','WY'];
     const parts = rawInput.split(",").map(str => str.trim());
     const lastPart = parts[parts.length - 1].toUpperCase();
+    //gets country code or state code like this
     const isUSState = US_STATES.includes(lastPart);
     const isCountryCode = parts.length === 3 || (!isUSState && parts.length === 2);
     lastQuery = isCountryCode ? parts.join(",") : `${parts.join(",")},US`;
-    
+    //this helps the API find accurate results especially if the user is searching outside of the US
 
     fetchAPIData(lastQuery);
     rawInput.value = '';
@@ -28,8 +26,10 @@ const fetchAPIData = async (query, units = 'imperial') => {
     }
 
     const key = 'ea12bceac54c6a464611413684fbe029';
+    showLoader();
+
     const base_API_URL = 'https://api.openweathermap.org/data/2.5';
-    const formattedQuery = encodeURIComponent(query);
+    const formattedQuery = encodeURIComponent(query); //formats query if user put in spaces for two word city names (ex: Los Angeles)
 
     const locationResponse = await fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${formattedQuery}&limit=1&appid=${key}`);
     const locationData = await locationResponse.json();
@@ -46,6 +46,7 @@ const fetchAPIData = async (query, units = 'imperial') => {
     const forecast = await forecastRes.json();
 
     const forecastArray = getDailyForecast(forecast);
+    hideLoader();
     createCard(current, forecastArray, region, units);
 };
 
@@ -71,8 +72,8 @@ const getDailyForecast = (forecast) => {
             }
         }
 
-        //Mid-day weather at this time might be most reprentative for the weather of the day
-        if(entry.dt_txt.includes('12:00:00')) {
+        //Open weather API gives the times in UTC
+        if(entry.dt_txt.includes('19:00:00')) {
             days[date].weather = entry.weather[0].main;
             days[date].iconID = entry.weather[0].icon;
         }
@@ -85,6 +86,7 @@ const getDailyForecast = (forecast) => {
 };
 
 const forecastHTML = (forecast) => {
+    console.log(forecast);
     const firstKey = Object.keys(forecast)[0];
     const firstDay = forecast[firstKey].day;
     const currentDay = getDay(new Date().toLocaleDateString("en-CA"));
@@ -93,6 +95,13 @@ const forecastHTML = (forecast) => {
     currentObjForecast = (firstDay === currentDay ? Object.values(forecast).slice(1) : Object.values(forecast)).slice(0, 5);
 
     //this was done because the forecast was going from a 5 day forecast to a 4 day or 6 day forecast without this, depending on the time of day
+
+    //this turns all places where it shows a "night" icon to a "day" icon
+    Object.keys(currentObjForecast).forEach(date => {
+        if(currentObjForecast[date].iconID === '01n') {
+            currentObjForecast[date].iconID = '01d';
+        }
+    });
 
     return currentObjForecast.map(day =>
         `<div class="forecast-day">
@@ -109,7 +118,7 @@ const forecastHTML = (forecast) => {
 const changingTheme = (sunrise, sunset) => {
     //doing the night theme
     const card = document.querySelector('.card');
-    const now = Date.now() / 1000;
+    const now = Date.now() / 1000; //converts to what the API uses
     const isNight = now < sunrise || now > sunset;
     //takes the time and compares it to the sunset and sunrise of the location, always reflecting the region's day time and night time in the theme
 
@@ -124,6 +133,7 @@ const changingTheme = (sunrise, sunset) => {
     }
 };
 
+//changing UI to reflect the units of temperature
 const boldTemp = (units) => {
     if(units === 'metric') {
         document.querySelector('.temp-bold-fah').style.fontWeight = '400';
@@ -154,7 +164,7 @@ const createCard = (current, forecast, region, units) => {
                             <div class="weather-data-column">
                                 <p>🌡️ Feels Like: ${Math.round(current.main.feels_like)}&deg;</p>
                                 <div>🔻 Min: ${Math.round(current.main.temp_min)}&deg; | 🔺 Max: ${Math.round(current.main.temp_max)}&deg;</div> 
-                                <p>💧 Humidity: ${current.main.humidity}% &middot | 💨 Wind: ${current.wind.speed} mi/h</p>                             
+                                <p>💧 Humidity: ${current.main.humidity}% &middot | 💨 Wind: ${units === "imperial" ? `${current.wind.speed} mph` : `${current.wind.speed} m/s`}</p>                             
                             </div>
                         </div>
                     </div>
@@ -171,6 +181,14 @@ const createCard = (current, forecast, region, units) => {
     document.querySelector('.temp-bold-cel').addEventListener('click', () => fetchAPIData(lastQuery, 'metric'));
     document.querySelector('.temp-bold-fah').addEventListener('click', () => fetchAPIData(lastQuery, 'imperial'));
 };
+
+const showLoader = () => {
+    document.querySelector('.loader').classList.add('show');
+};
+
+const hideLoader = () => {
+    document.querySelector('.loader').classList.remove('show');
+}
 
 
 //Event Listeners
